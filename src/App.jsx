17e594721b414f5790ffbc0fa1e6cdece2bfcb0c
Eduardo <el_alienx@hotmail.com>
2023-02-28 10:42:29 +0100

    finalize exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+// Project files
+import { downloadFile, uploadFile } from "./scripts/cloudStorage";
+
 export default function App() {
+  // Methods
+  async function onChange(event) {
+    const file = event.target.files[0];
+    const filePath = `new-folder/${Date.now()}_${file.name}`;
+    let url = "";
+
+    await uploadFile(file, filePath);
+    url = await downloadFile(filePath);
+
+    console.log("From React url", url);
+    console.log(Date.now());
+  }
+
   return (
     <div className="App">
       <h1>Firebase - Cloud Storage</h1>
+
+      <label>
+        Image chooser
+        <input
+          type="file"
+          accept="image/png, image/jpeg, image/webp"
+          onChange={(event) => onChange(event)}
+        />
+      </label>
+      <label>
+        File chooser
+        <input type="file" onChange={(event) => onChange(event)} />
+      </label>
     </div>
   );
 }
---
+++ src/scripts/firebaseSetup.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const setup = {
+  apiKey: "AIzaSyDveHYg89r_GInm6hDPbqUpbxHVtAmggBU",
+  authDomain: "fe3-spike-firestore.firebaseapp.com",
+  projectId: "fe3-spike-firestore",
+  storageBucket: "fe3-spike-firestore.appspot.com",
+  messagingSenderId: "175225435910",
+  appId: "1:175225435910:web:750da1721d3ce9f3af5087",
+};
+
+const firebaseApp = initializeApp(setup);
+
+export const cloudStorage = getStorage(firebaseApp);
