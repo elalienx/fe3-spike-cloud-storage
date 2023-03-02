1c1d44ef59f337fd050a98a0499503dac3a38841
Eduardo <el_alienx@hotmail.com>
2023-03-02 09:54:05 +0100

    preparing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 // Project files
 import { useState } from "react";
-import { downloadFile, uploadFile } from "./scripts/cloudStorage";
+import { downloadFile, uploadFile } from "./scripts/firebase/cloudStorage";
+import readFile from "./scripts/resize-image/readFile";
+import resizedImage from "./scripts/resize-image/resizeImage";
 
 export default function App() {
-  const [imageToUpload, setImageToUpload] = useState("");
   // Methods
   async function onChange(event) {
     const file = event.target.files[0];
+    console.log("file", file);
+
     const filePath = `new-folder/${Date.now()}_${file.name}`;
 
     setImageToUpload(file);
   }
+
+  // Impure (1, 4)
+  // async function onAddImage(event, item, editItem) {
+  //   const file = event.target.files[0];
+  //   const uniqueId = new Date().getTime();
+  //   const filename = `thumbnail-${uniqueId}.png`;
+  //   const image = await readFile(file);
+  //   const resizedImage = await resizeImage(image, 88, 88);
+  //   const imageURL = await uploadFile(cloudReference, resizedImage, filename);
+  //   const clonedItem = { ...item };
+
+  //   clonedItem.imageURL = imageURL;
+  //   editItem(clonedItem);
+  // }
 
   return (
     <div className="App">
@@ -23,10 +40,6 @@ export default function App() {
           accept="image/png, image/jpeg, image/webp"
           onChange={(event) => onChange(event)}
         />
-      </label>
-      <label>
-        File chooser
-        <input type="file" onChange={(event) => onChange(event)} />
       </label>
     </div>
   );
